d958d92088bfdb572de8377cd933e15a5f379711
kwchi <vladislava.pa@gmail.com>
2023-09-11 23:02:55 +0300

    first commit

---
+++ lab1_2/1st.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Example
+{
+    public static void Main()
+    {
+        Random rnd = new Random();
+        for (int ctr = 0; ctr < 10; ctr++)
+        {
+            Console.Write("{0,3} ", rnd.Next(-10, 11));
+        }
+    }
+}
---
+++ lab_ifb/2nd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            byte[] randomBytes = new byte[32]; 
+
+            rng.GetBytes(randomBytes);
+
+            string randomHex = BitConverter.ToString(randomBytes).Replace("-", "");
+
+            Console.WriteLine(randomHex);
+        }
+    }
+}
