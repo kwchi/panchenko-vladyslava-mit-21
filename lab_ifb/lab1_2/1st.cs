f99d32e090274641ee26ca5f747b22919d618cfd
kwchi <144099685+kwchi@users.noreply.github.com>
2023-11-20 01:05:56 +0200

    oinfb_lab1

---
+++ lab_ifb/lab1_2/1st.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Example
+{
+    public static void Main()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            Random rnd = new Random(5234);
+            for (int ctr = 0; ctr < 10; ctr++)
+            {
+                Console.Write("{0,3} ", rnd.Next(-10, 11));
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lab_ifb/lab_ifb/2nd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomBytes = new byte[32];
+
+                rng.GetBytes(randomBytes);
+
+                string randomHex = BitConverter.ToString(randomBytes).Replace("-", " ");
+
+                Console.WriteLine(randomHex);
+            }
+        }
+    }
+}
